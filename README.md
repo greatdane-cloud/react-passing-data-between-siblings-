58841992ee11782606fe3453fdc701b9a8bf1a96
greatdane-cloud <vanderbyljeandre@gmail.com>
2022-08-08 10:14:52 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ucvsa2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ucvsa2)
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import './style.css';
+import Sidebar from './Sidebar.js';
+import Home from './Home.js';
+
+function App() {
+  const [selectedMode, setSelectedMode] = useState('open');
+  return (
+    <div className="app">
+      <Sidebar setSelectedMode={setSelectedMode} />
+      <Home selectedMode={selectedMode} />
+    </div>
+  );
+}
+
+export default App;
